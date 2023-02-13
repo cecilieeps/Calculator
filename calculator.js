f829900c3825f4da1b8e6621952849884ba38efd
cecilieeps <csande993@gmail.com>
2023-02-13 07:10:21 +0100

    Add buttons and define operator functions.

---
+++ calculator.js
@@ -0,0 +1,36 @@
+// loop over all nodes in the buttons class and add an event
+const buttons = document.querySelectorAll('.buttons');
+const display = document.querySelector('.display');
+// const operatorSelected 
+
+
+function add(a, b){
+    return a+b;
+}
+
+function subtract(a, b){
+    return a-b;
+}
+
+function multiply(a, b){
+    return a*b;
+}
+
+function divide(a, b){
+    return a/b;
+}
+
+function operate(operator, a, b) {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            break;
+    }
+}
