67dafd5c78f90c3aa4dcc42add59ba3ad2c8a491
cecilieeps <cecilieeps@users.noreply.github.com>
2023-02-14 23:56:16 +0100

    Allow a single operation to be performed.

--- calculator.js
+++ calculator.js
@@ -1,14 +1,7 @@
-// loop over all nodes in the buttons class and add an event
-const buttons = document.querySelectorAll('.buttons');
-let displayValue = '0';
-let value = '';
 let operator;
-// the value of a will be updated continuously as it will be passed to the functions
-// the value the calculator starts with
 let a; 
 let b;
-// what will be displayed above current value 
-let values = "";
+let result;
 
 const operators = document.querySelectorAll('.operator');
 const numbers = document.querySelectorAll('.number');
@@ -17,71 +10,99 @@ const mainDisplay = document.querySelector('.main-display');
 const equals = document.querySelector('.equals');
 
 equals.addEventListener('click', () => {
-
-})
+    if (a && b && operator) {
+        smallDisplay.textContent += '=';
+        result = operate(operator, a, b); // sets operator to ''
+        mainDisplay.textContent = result;
+        console.log(mainDisplay.textContent);
+        console.log("equals");
+        // now update a to be the result of the above operation
+    }
+    a = result;
+    b = '';
+});
 
 operators.forEach(o => {
     o.addEventListener('click', () => {
-        value += o.textContent;
         operator = o.textContent;
-        updateDisplay();
+        if (a && !b) {
+            smallDisplay.textContent += operator;
+        }
+        if (a && b) {
+            console.log(a);
+            console.log(b);
+            /* if a and b both have values assigned and an operator has been clicked on
+            we need to update the operator value and perform operation on a, b.
+            Change the small display value and the main value to the result and add the new operator to the new value.
+            */
+            smallDisplay.textContent = operate(a, b, operator);
+            mainDisplay.textContent = operate(a, b, operator);
+        }
     });
 });
 
-function updateDisplay() {
-    mainDisplay.textContent = value;
+function updateDisplay(x) {
+    smallDisplay.textContent += x;
 }
 
 numbers.forEach(number => {
     number.addEventListener('click', () => {
-        value += number.textContent; // already a string
+        /* a number has been clicked on. Check if a has a value, update it with the number thats been clicked on if it doesnt.
+        */
         if (!a) {
-            a = number;
+            a = number.textContent;
+            mainDisplay.textContent = number.textContent;
+            smallDisplay.textContent = number.textContent;
+        }
+        else if (!b) {
+            preceedingValue = a;
+            b = number.textContent;
+            mainDisplay.textContent = number.textContent;
+            smallDisplay.textContent += number.textContent;
         }
         else {
-            b = number; // after b is assigned a value then we can perform the operation
-            value = operate(operator, a, b);
+            /*
+            If another operator is clicked on while a, b have values
+            then automatically do the operation on a, b and then update
+            the operator variable. 
+            */
         }
-        updateDisplay();
     });
 })
-
-console.log(value);
 
 // whenever an operation is performed, we need to update the display
 // to be equal to the answer here;
 function add(a, b){
-    value = a + b;
-    return a+b;
+    return Number(a)+Number(b);
 }
 
 function subtract(a, b){
-    value = a - b;
     return a-b;
 }
 
 function multiply(a, b){
-    value = a * b;
     return a*b;
 }
 
 function divide(a, b){
-    value = a/b;
     return a/b;
 }
 
-// a will be the previous stored value
-// whenever this is called, add some string to values variable i.e. what will be
-// displayed above the result 
 function operate(operator, a, b) {
     switch (operator) {
         case '+':
+            // reset operator
+            operator = '';
+            console.log("in operate");
             return add(a, b);
         case '-':
+            operator = '';
             return subtract(a, b);
         case '*':
+            operator = '';
             return multiply(a, b);
-        case '/':
+        case '÷':
+            operator = '';
             return divide(a, b);
         default:
             break;
